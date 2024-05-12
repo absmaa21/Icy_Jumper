5a9998c01622b9b6c7d4957cb4554bbb85cd493c
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2024-05-12 13:57:03 +0200

    Updated player mid-air movement
    Player now cannot move as fast in air if direction was changed one time

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,44 +6,49 @@ const JUMP_VELOCITY = -500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var latest_jump_load_start = -1.0
 var is_jump_loading = false
+var deltaTimeSinceStart = 0
+var changedDirectionInAir = false
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 
 func _physics_process(delta):
-	
 	# Add the gravity.
 	if not is_on_floor():
 		animated_sprite.play("jump")
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		is_jump_loading = true
 		animated_sprite.play("jump_load")
-		latest_jump_load_start = Time.get_ticks_msec()
-	if Input.is_action_just_released("jump") and is_on_floor() and latest_jump_load_start > -1:
+		deltaTimeSinceStart += delta
+	if Input.is_action_just_released("jump") and is_on_floor():
 		is_jump_loading = false
-		var time_since_load_jump = (Time.get_ticks_msec() - latest_jump_load_start) / 1000.0
-		print(time_since_load_jump)
-		var jump_power = 0
-		if time_since_load_jump > 1:
-			jump_power = 1
-		elif time_since_load_jump < 0.3:
-			jump_power = 0.3
-		else:
-			jump_power = time_since_load_jump
+		print(deltaTimeSinceStart)
+		var jump_power = clamp(deltaTimeSinceStart, 0.1, 0.7)
 		velocity.y = JUMP_VELOCITY * jump_power
-		latest_jump_load_start = -1
-		
+		deltaTimeSinceStart = 0
+	
+	if not is_on_floor() and (Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right")):
+		changedDirectionInAir = true
+	
 	var direction = Input.get_axis("left", "right")
-	if is_on_floor() and not latest_jump_load_start > 0:
-		velocity.x = 0
+	if is_on_floor() and not deltaTimeSinceStart > 0:
+		resetMoveVelocity()
 		animated_sprite.play("idle")
-	elif not is_jump_loading:
+	elif not is_jump_loading and not is_on_floor():
 		if direction:
-			velocity.x = direction * SPEED
+			if changedDirectionInAir:
+				velocity.x = direction * 0.4 * SPEED
+			else:
+				velocity.x = direction * SPEED
+	else:
+		resetMoveVelocity()
 
 	move_and_slide()
+
+func resetMoveVelocity():
+	velocity.x = 0
+	changedDirectionInAir = false
