ef4fe822fa8809a1e0d89b43cd02bcd0cba76fc7
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2024-05-13 21:31:16 +0200

    Add sounds/music/ambient
    Sounds: Player jump & fall from high
    Music: Cave Theme
    Ambient: Water dripping & dungeon ambient

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,15 +13,29 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jump_loading = false
 var deltaTimeSinceStart = 0
 var changedDirectionInAir = false
+var isFallingFromHigh = false
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var fall_time = $FallTime
+
+@onready var jump_sound = $Sounds/Jump
+@onready var fall_onto_ground_sound = $Sounds/FallOntoGround
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		animated_sprite.play("jump")
+		if velocity.y < 0: # is jumping
+			animated_sprite.play("jump")
+		elif velocity.y > 0: # is falling
+			if fall_time.is_stopped() or fall_time.paused:
+				fall_time.start()
+			animated_sprite.play("fall")
 		velocity.y += gravity * delta
+
+	# Handle Landing
+	if is_on_floor() and isFallingFromHigh:
+		handleLandingEffects()
 
 	# Handle jump.
 	if Input.is_action_pressed("jump") and is_on_floor():
@@ -34,17 +48,17 @@ func _physics_process(delta):
 		var jump_power = clamp(deltaTimeSinceStart, min_jump_charge_time, max_jump_charge_time)
 		velocity.y = jump_velocity * jump_power
 		deltaTimeSinceStart = 0
-	
+		jump_sound.play()
+
 	# Handle air movement
 	if not is_on_floor() and (Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right")):
 		changedDirectionInAir = true
-	
+
 	# Handle movement
 	var direction = Input.get_axis("left", "right")
 	changeSpriteDirection(direction)
 	if is_on_floor() and not deltaTimeSinceStart > 0:
 		resetMoveVelocity()
-		animated_sprite.play("idle")
 	elif not is_jump_loading and not is_on_floor():
 		if direction:
 			if changedDirectionInAir:
@@ -56,11 +70,23 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+func handleLandingEffects():
+	if isFallingFromHigh:
+		fall_onto_ground_sound.play()
+		animated_sprite.play("fell_from_high")
+	isFallingFromHigh = false
+
 func resetMoveVelocity():
+	if animated_sprite.animation != 'fell_from_high' and not is_jump_loading:
+		animated_sprite.play("idle")
 	velocity.x = 0
 	changedDirectionInAir = false
+	fall_time.stop()
 
 func changeSpriteDirection(direction):
+	if animated_sprite.animation == 'fell_from_high':
+		return
+
 	if animated_sprite.flip_h:
 		if direction <= 0:
 			animated_sprite.flip_h = true
@@ -72,3 +98,6 @@ func changeSpriteDirection(direction):
 			animated_sprite.flip_h = false
 		else:
 			animated_sprite.flip_h = true
+
+func _on_fall_time_timeout():
+	isFallingFromHigh = true
